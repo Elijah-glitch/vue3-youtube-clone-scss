89ed0d6f168b7f5143be6414f9cf390a08efe5fd
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-08-21 23:29:40 +0300

    PrimaryButton.vue; PrimaryButton added.

---
+++ src/components/Input/Button/Primary/PrimaryButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button @click="onClick()" class="button"><slot /></button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PrimaryButton",
+  props: {
+    onClick: {
+      type: Function,
+      default: () => true,
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: var(--yt-red);
+  padding: 10px 16px;
+}
+</style>
